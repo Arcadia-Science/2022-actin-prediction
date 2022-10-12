0ee0116cbed9d028f1a6dbb5a5908a0caaaa4db9
Taylor Reiter <taylorreiter@gmail.com>
2022-10-12 13:02:43 -0400

    update test source call for read feature csv

--- tests/test-read_feature_csv.R
+++ tests/test-read_feature_csv.R
@@ -1,5 +1,5 @@
 test_that("check that no NAs sneak in to the feature df", {
-  source("../R/read_feature_df.R")
+  source("../R/read_feature_csv.R")
   df <- read_feature_csv("../inputs/protein_features/atp_binding.csv")
   expect_equal(nrow(df[complete.cases(df), ]), nrow(df))
 })
